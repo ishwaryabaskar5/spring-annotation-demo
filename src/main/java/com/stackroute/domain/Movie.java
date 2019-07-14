a47a7d45ec89e082c2aecdd6b9755efe8b880e34
isbaska <isbaska1@in.ibm.com>
2019-07-14 13:58:53 +0530

    Task 2 is implemented

--- src/main/java/com/stackroute/domain/Movie.java
+++ src/main/java/com/stackroute/domain/Movie.java
@@ -4,12 +4,15 @@ public class Movie {
 	
 	private Actor actor;
 	
-	public void setActor(Actor actor) {
-		this.actor = actor;
+	public Movie() {
 	}
 	
+	public Movie(Actor actor) {
+		this.actor = actor;
+	}
+
 	public void display()
 	{
-		System.out.println("Actor Name: "+actor.getName()+" Gender: "+actor.getGender()+"Age: "+actor.getAge() );
+		System.out.println("Actor Name: "+actor.getName()+" Gender: "+actor.getGender()+" Age: "+actor.getAge() );
 	}
 }
